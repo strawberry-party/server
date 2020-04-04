bab4c4e9f30a38afb04129c20541b07d4ead50a6
GyubinLee <leegyubin00@gmail.com>
2020-04-05 07:56:03 +0900

    for testing

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4500;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/api/message', (req, res) => {
