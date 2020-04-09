ccf25a1e36625a3f16366680f0b6e4b52ad94bf0
GyubinLee <leegyubin00@gmail.com>
2020-04-09 13:22:13 +0900

    CORS

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var cors = require('cors');
+
 const app = express();
-const port = process.env.PORT || 4500;
+const port = process.env.PORT || 5001;
+
+app.use(cors())
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/api/message', (req, res) => {
